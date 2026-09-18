6c6953c7b512905f3d8850634cac4ee3075777ca
Sudharani Biradar <sudharanibiradr970@gmail.com>
2026-09-18 15:34:57 +0530

    Sheet2 started

--- Sheet2.sql
+++ Sheet2.sql
@@ -1,1 +1,10 @@
 USE sql_practice;
+-- Creating Employees Table
+CREATE TABLE Employees(emp_id INT PRIMARY KEY,
+						emp_name VARCHAR(50),
+						salary INT,
+						dept_id VARCHAR(10),
+						manager_id VARCHAR(10),
+						hire_date DATE,
+						email VARCHAR(100)
+                        );
